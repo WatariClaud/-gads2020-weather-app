db1a535a23a1ad1eb44148b8159ac7f56b9b7568
Claud Watari <claudwatari95@gmail.com>
2020-08-30 00:24:45 +0300

    Optimize service worker v1

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then((reg) => {
+    // registration worked
+        console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch((error) => {
+    // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
+
 document.querySelector('#date-year').textContent = new Date().getFullYear();
 const cityName =  document.querySelector('#city-title');
 const searchBox = document.querySelector('#inputbox');
