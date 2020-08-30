a7e8b1b4e0dae117725d8b666d436129b78f12c3
Claud Watari <claudwatari95@gmail.com>
2020-08-30 05:01:06 +0300

    Add capital cities dropdown

--- main.js
+++ main.js
@@ -17,12 +17,14 @@ const searchBox = document.querySelector('#userquery');
 const searchBtn = document.querySelector('#search-btn');
 const resLoader = document.querySelector('#load-container');
 const cityTitle = document.querySelector('#city-title strong');
-const cityContent = document.querySelector('#city-content strong');
+const cityContent = document.querySelector('#city-content');
 const weatherContent = document.querySelector('#weather-content');
-const tempContentHi = document.querySelector('#temp-content-hi');
-const tempContentLow = document.querySelector('#temp-content-low');
+const tempContent = document.querySelector('#temp-content');
 const atmTitle = document.querySelector('#atm-title strong');
 const atmContent = document.querySelector('#atm-content');
+const humidityContent = document.querySelector('#humidity-content');
+const descriptionContent = document.querySelector('#description-content');
+const locationsList = document.querySelector('#locations');
 
 const loadRes = () => {
     resContent.classList.remove('hidden')
@@ -36,42 +38,54 @@ const getCurrentWeatherDefault = () => {
     fetch('http://ip-api.com/json')
     .then((r) => r.json())
     .then((r) =>{
-         console.log(r);
-         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${r.city}&APPID=503ddc3764a990cdaff00121f6840069`)
+         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${r.city}&units=metric&APPID=503ddc3764a990cdaff00121f6840069`)
          .then(result => result.json())
          .then((r) => {
-             console.log(r);
             cityContent.textContent = `${r.name}, ${r.sys.country}`;
             weatherContent.textContent = r.weather[0].main;
-            tempContentHi.textContent = `${r.main.temp_max}°`;
-            tempContentLow.textContent = `${r.main.temp_min}°`;
-            if(r.rain) atmTitle.textContent = 'Rain',atmContent.textContent = `${r.rain['1h']}`;
-            if(r.wind) atmTitle.textContent = 'Wind',atmContent.textContent = `${r.wind['speed']}`;
+            tempContent.textContent = `${r.main.temp}°C`;
+            humidityContent.textContent = `${r.main.humidity}%`;
+            descriptionContent.textContent = `${r.weather[0].description}`;
+            // tempContentLow.textContent = `${r.main.temp_min}°C`;
+            if(r.rain) atmTitle.textContent = 'Rain:', atmContent.textContent = `${r.rain['1h']} mm`;
+            if(r.wind) atmTitle.textContent = 'Wind:', atmContent.textContent = `${r.wind['speed']} m/s`;
          })
     })
     .catch((e) => console.log(e));
 }
+const getListOfLocations = () => {
+    fetch('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json')
+    .then((l) => l.json())
+    .then((l) => {
+        l.forEach(location => {
+            locationsList.innerHTML += `<option value = '${location.city}'>${location.country}'</option>`
+        });
+    })
+}
 getCurrentWeatherDefault();
+getListOfLocations();
 
 const showRes = (event) => {
     event.preventDefault();
+    if(searchBox.value === '' || !searchBox.value) return searchBox.classList.add('err'), searchBox.focus(), false;
     resContent.classList.add('hidden')
     resLoader.classList.remove('hidden');
     
     setTimeout(loadEvent, 500);
     let cityQuery;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchBox.value}&APPID=503ddc3764a990cdaff00121f6840069`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchBox.value}&units=metric&APPID=503ddc3764a990cdaff00121f6840069`)
     .then(result => result.json())
     .then(result => {
-        console.log(result);
         cityQuery = result.name;
         cityTitle.textContent = 'Result for:'
         cityContent.textContent = `${cityQuery}, ${result.sys.country}`;
         weatherContent.textContent = result.weather[0].main;
-        tempContentHi.textContent = `${result.main.temp_max}°`;
-        tempContentLow.textContent = `${result.main.temp_min}°`;
-        if(result.rain) atmTitle.textContent = 'Rain',atmContent.textContent = `${result.rain['1h']}`;
-        if(result.wind) atmTitle.textContent = 'Wind',atmContent.textContent = `${result.wind['speed']}`;
+        tempContent.textContent = `${result.main.temp}°C`;
+        humidityContent.textContent = `${result.main.humidity}%`;
+        descriptionContent.textContent = `${result.weather[0].description}`;
+        // tempContentLow.textContent = `${result.main.temp_min}°C`;
+        if(result.rain) atmTitle.textContent = 'Rain:',atmContent.textContent = `${result.rain['1h']} mm`;
+        if(result.wind) atmTitle.textContent = 'Wind:',atmContent.textContent = `${result.wind['speed']} m/s`;
     })
     .catch(err => console.log(err));
 }
